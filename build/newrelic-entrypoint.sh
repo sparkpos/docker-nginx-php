3b1afdf6fabcdb4c865b18439f504a1e856d31b0
terry zhang <zterry@qq.com>
2017-12-11 03:44:07 +0800

    fix newrelic

--- build/newrelic-entrypoint.sh
+++ build/newrelic-entrypoint.sh
@@ -3,3 +3,5 @@
 export NR_INSTALL_SILENT=true
 sed -i "s/newrelic.appname = .*/newrelic.appname = \"$NEWRELICAPPNAME\"/" /etc/php/7.1/fpm/conf.d/newrelic.ini
 sed -i "s/newrelic.license = .*/newrelic.license = \"$NEWRELICKEY\"/" /etc/php/7.1/fpm/conf.d/newrelic.ini
+
+exec "$@"
