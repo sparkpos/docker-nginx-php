4dd07f41a470e47409d14de838651b40dd0f4204
terry zhang <zterry@qq.com>
2017-12-10 21:39:29 +0800

    refact newrelic

--- startup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-Nginx_Install_Dir=/usr/local/nginx
-DATA_DIR=/var/www/html
-PHP_EXT=/var/www/phpext
-
-set -e
-chown -R www-data:www-data $DATA_DIR
-/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
