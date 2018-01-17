819d73bcb6b10df8c1dd215e636653b1723ee3b0
lixiuhu <xhli@sparkpad.com>
2018-01-17 13:02:56 +0800

    Reconstruction of organizational structure

---
+++ 7.1/baseimage/supervisor/supervisor-service.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+export NR_INSTALL_SILENT=true
+sed -i "s/newrelic.appname = .*/newrelic.appname = \"$NEWRELICAPPNAME\"/" /etc/php/7.1/fpm/conf.d/newrelic.ini
+sed -i "s/newrelic.license = .*/newrelic.license = \"$NEWRELICKEY\"/" /etc/php/7.1/fpm/conf.d/newrelic.ini
+
+/usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
