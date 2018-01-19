9c55eade9bd62d38b0eb29855e2d66ebc582e4a0
lixiuhu <xhli@sparkpad.com>
2018-01-19 20:03:47 +0800

    fix newrelic.sh problem

--- 7.1/alpine/newrelic/newrelic.sh
+++ 7.1/alpine/newrelic/newrelic.sh
@@ -4,11 +4,8 @@ if [ ! -z ${NEWRELIC_KEY} ];
 then 
   if [ ! -z ${NEWRELIC_APPNAME} ];
   then
-    
-    sed -i \
-        -e "s/newrelic.license =.*/newrelic.license = \${NEWRELIC_KEY}/" \
-        -e "s/newrelic.appname =.*/newrelic.appname = \${NEWRELIC_APPNAME}/" \
-        /etc/php7/conf.d/newrelic.ini
+    sed -i "s/newrelic.license =.*/newrelic.license = ${NEWRELIC_KEY}/g" /etc/php7/conf.d/newrelic.ini 
+    sed -i "s/newrelic.appname =.*/newrelic.appname = ${NEWRELIC_APPNAME}/g" /etc/php7/conf.d/newrelic.ini  
   fi
 fi
 
