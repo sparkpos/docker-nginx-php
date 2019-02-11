8a3e0a42aef6ed231213ecf34d4525c7bab4f76e
terry zhang <zterry@qq.com>
2019-02-11 15:09:14 +0800

    add env PHP_FPM_STATUS_ENABLE

--- conf/docker-entrypoint.sh
+++ conf/docker-entrypoint.sh
@@ -4,7 +4,7 @@ mv /tmp/$APP.conf /etc/nginx/site-enabled/
 
 # Increase the memory_limit
 if [ ! -z "$PHP_MEM_LIMIT" ]; then
- sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" /usr/local/etc/php/php.ini
+  sed -i "s/memory_limit = 128M/memory_limit = ${PHP_MEM_LIMIT}M/g" /usr/local/etc/php/php.ini
 fi
 
 # Increase the post_max_size
@@ -12,6 +12,11 @@ if [ ! -z "$MAX_FILE_UPLOAD_SIZE" ]; then
   sed -i "s/post_max_size = 32M/post_max_size = ${MAX_FILE_UPLOAD_SIZE}M/g" /usr/local/etc/php/php.ini
   sed -i "s/upload_max_filesize = 32M/upload_max_filesize= ${MAX_FILE_UPLOAD_SIZE}M/g" /usr/local/etc/php/php.ini
   sed -i "s/client_max_body_size 30M;/client_max_body_size ${MAX_FILE_UPLOAD_SIZE}M;/g" /etc/nginx/nginx.conf
+fi
+
+# fpm status
+if [ ! -z "$PHP_FPM_STATUS_ENABLE" ]; then
+  sed -i "s/;pm.status_path = \/status/pm.status_path = \/status/g" /usr/local/etc/php-fpm.d/www.conf
 fi
 
 exec "$@"
