1bb603da20eed0033669df2db5fe9fb07c9dbebc
terry zhang <zterry@qq.com>
2021-06-15 23:46:05 +0000

    8.1-rc support

--- update.sh
+++ update.sh
@@ -8,8 +8,11 @@ sedStr="
 sed -r "$sedStr" $1
 }
 
-versions=(7.1 7.2 7.3 7.4)
+versions=(7.1 7.2 7.3 7.4 8.1-rc)
 for version in ${versions[*]}; do
+  if [ ! -d ${version}/alpine ]; then
+    mkdir -p ${version}/alpine
+  fi
   render Dockerfile-alpine.template > $version/alpine/Dockerfile
   # https://github.com/flyve-mdm/docker-environment/issues/68
   if [ "${version}" = "7.1" ]; then
@@ -17,7 +20,7 @@ for version in ${versions[*]}; do
   fi
   # https://www.php.net/manual/en/image.installation.php
   # php 7.4 gd config differenct as before.
-  if [ "${version}" = "7.4" ]; then
+  if [[ "${version}" = "7.4" || "${version}" = "8.1-rc" ]]; then
     sed -i "s/with-gd/enable-gd/g" ${version}/alpine/Dockerfile
     sed -i "s/--with-png-dir=\/usr\/include\///g" ${version}/alpine/Dockerfile
     sed -i "s/-dir=/=/g" ${version}/alpine/Dockerfile
